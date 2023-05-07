61a1d05e0cda53d44ec499cf10fb03ac199a7f7e
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-05-07 08:16:48 +0300

    routing update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Switch, Route} from "react-router-dom";
+
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+
+import MemberList from "./components/MemberList";
+import MemberForm from "./components/MemberForm";
 
 const App = () => {
   return (
     <>
-      App
+      <Header/>
+
+      <Switch>
+        <Route exact path="/"><MemberList/></Route>
+
+        <Route path="/ekle"><MemberForm/></Route>
+      </Switch>
+
+      <Footer/>
     </>
   )
 }
